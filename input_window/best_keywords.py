f1db306ef1ddf394d1732302af619a851ecdf021
evgeniy <pavlinbl4@gmail.com>
2023-08-19 15:07:26 +0300

    now "bad words" used from txt file

--- input_window/best_keywords.py
+++ input_window/best_keywords.py
@@ -1,9 +1,20 @@
 import re
+from input_window.bad_words_from_file import get_bad_words_from_txt_file
 
-words_to_remove = r'_РЕЛИГИЯ|_гипермаркеты|_фирмы|_ПРЕДМЕТ_|_People|_ГОРОДА И СТРАНЫ_|_PERSONS|_CITY|_ИМЯ СОБСТВЕННОЕ_|COUNTRY|_РУССКИЙ_|_ГОРОДА И СТРАНЫ|\|'
+# words_to_remove = r'_РЕЛИГИЯ|_гипермаркеты|_фирмы|_ПРЕДМЕТ_|_People|_ГОРОДА И СТРАНЫ_|_PERSONS|_CITY|_ИМЯ СОБСТВЕННОЕ_|COUNTRY|_РУССКИЙ_|_ГОРОДА И СТРАНЫ|\|'
 
 
-def keywords_opimization(string):
+# def add_words_to_txt(path_to_txt):  # temp function to add words to file
+#     with open(path_to_txt, 'a') as txt_file:
+#         for i in words_to_remove.lstrip().split('|'):
+#             # txt_file.writelines(words_to_remove.lstrip().split('|'))
+#             if len(i) > 2:
+#                 txt_file.write(i + '\n')
+
+
+def keywords_opimization(string, path_to_bad_words_file='/Users/evgeniy/Documents/keywords/bad_words.txt'):
+    words_to_remove = get_bad_words_from_txt_file(path_to_bad_words_file)
+
     # remove bad words
     no_bad_words = re.sub(words_to_remove, "", string).strip()
 
@@ -23,3 +34,5 @@ def keywords_opimization(string):
         result = ', '.join(words)
 
     return result
+
+
