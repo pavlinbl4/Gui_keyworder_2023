014e5d25e7439755962ebafc45c32e9b280536d1
evgeniy_MacBook <pavlinbl4@gmail.com>
2023-08-21 18:20:15 +0300

    gui tool for synomys

--- synonims/4_ru_synonyms.py
+++ synonims/4_ru_synonyms.py
@@ -6,6 +6,7 @@ import pyperclip
 
 from Common.cvs_writer import save_to_csv
 from input_window.checkbox_output import create_checkbox_list
+from input_window.new_input_window import create_input_window
 
 # Initialize both synonyms and antonyms graph
 sg = SynonymsGraph()
@@ -16,11 +17,21 @@ def get_synonyms(to_word: str) -> dict:
     synonyms_list = [synonim for synonim in sg.get_list(to_word)]
     selected_synonyms = create_checkbox_list(synonyms_list, "Select synonims")
     synonyms_str = ", ".join(selected_synonyms)
-    save_to_csv([['Main word', 'Synonyms'], [to_word, synonyms_str]],
+    save_to_csv([[to_word, synonyms_str]],
                 '/Users/evgeniy/Documents/keywords/synonyms.csv')
     pyperclip.copy(synonyms_str)
     return selected_synonyms
 
 
+def main():
+    to_word = create_input_window()[0]
+    get_synonyms(to_word)
+
+
+
+
 if __name__ == '__main__':
-    print(get_synonyms('коррупция'))
+
+    # print(get_synonyms('бизнесмен'))
+
+    main()
