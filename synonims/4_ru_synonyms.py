ff2ff4315636b1f42d88122217c7858ebe75a5de
evgeniy_MacBook <pavlinbl4@gmail.com>
2023-08-21 13:36:30 +0300

    add copy rezult to clipboard

--- synonims/4_ru_synonyms.py
+++ synonims/4_ru_synonyms.py
@@ -2,20 +2,25 @@
 
 
 from ru_synonyms import SynonymsGraph
+import pyperclip
 
+from Common.cvs_writer import save_to_csv
 from input_window.checkbox_output import create_checkbox_list
 
 # Initialize both synonyms and antonyms graph
 sg = SynonymsGraph()
 
 
-def get_synonyms(to_word:str)-> dict:
+def get_synonyms(to_word: str) -> dict:
     assert sg.is_in_dictionary(to_word)
     synonyms_list = [synonim for synonim in sg.get_list(to_word)]
     selected_synonyms = create_checkbox_list(synonyms_list, "Select synonims")
-    # return dict with main word as key and synonims list as item
-    return {to_word: selected_synonyms}
+    synonyms_str = ", ".join(selected_synonyms)
+    save_to_csv([['Main word', 'Synonyms'], [to_word, synonyms_str]],
+                '/Users/evgeniy/Documents/keywords/synonyms.csv')
+    pyperclip.copy(synonyms_str)
+    return selected_synonyms
 
 
 if __name__ == '__main__':
-    print(get_synonyms('ключ'))
+    print(get_synonyms('коррупция'))
