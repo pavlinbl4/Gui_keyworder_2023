2a919c9ba9eac1ea4e831fcc78caf1ad81483313
evgeniy <pavlinbl4@gmail.com>
2023-08-22 22:02:54 +0300

    keyword writer

--- keyworder/write_keywords.py
+++ keyworder/write_keywords.py
@@ -1,8 +1,17 @@
 from datetime import datetime
+import os
 
 
-def write_keywords(final, keyword_file):
-    with open(keyword_file, 'a') as text_file:
+def write_keywords(final, file_path: str):
+    folder = os.path.dirname(file_path)
+    if not os.path.exists(folder):
+        os.makedirs(folder)
+    with open(file_path, 'a') as text_file:
         text_file.write('\n')
         text_file.write(datetime.now().strftime("%Y-%m-%d") + '\n')
         text_file.write(final)
+
+
+if __name__ == '__main__':
+    write_keywords("one, two, tree", '/Volumes/big4photo/Documents/Kommersantttt/text.txt')
+
