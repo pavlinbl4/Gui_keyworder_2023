b4de038082359b685352f776dc80e73eedc3cd63
evgeniy_MacBook <pavlinbl4@gmail.com>
2023-08-22 09:38:15 +0300

    refactoring

--- synonims/4_ru_synonyms.py
+++ synonims/4_ru_synonyms.py
@@ -5,17 +5,17 @@ from ru_synonyms import SynonymsGraph
 import pyperclip
 
 from Common.cvs_writer import save_to_csv
-from input_window.checkbox_output import create_checkbox_list
-from input_window.new_input_window import create_input_window
+from KW_2023_08.checkbox_output import create_checkbox_list
+from KW_2023_08.new_input_window import create_input_window
 
 # Initialize both synonyms and antonyms graph
 sg = SynonymsGraph()
 
 
-def get_synonyms(to_word: str) -> dict:
+def get_synonyms(to_word: str) -> list:
     assert sg.is_in_dictionary(to_word)
     synonyms_list = [synonim for synonim in sg.get_list(to_word)]
-    selected_synonyms = create_checkbox_list(synonyms_list, "Select synonims")
+    selected_synonyms = create_checkbox_list(synonyms_list, "Select synonyms")
     synonyms_str = ", ".join(selected_synonyms)
     save_to_csv([[to_word, synonyms_str]],
                 '/Users/evgeniy/Documents/keywords/synonyms.csv')
@@ -24,14 +24,11 @@ def get_synonyms(to_word: str) -> dict:
 
 
 def main():
-    to_word = create_input_window()[0]
+    to_word = create_input_window("Enter word")[0]
     get_synonyms(to_word)
 
 
-
-
 if __name__ == '__main__':
-
     # print(get_synonyms('бизнесмен'))
 
     main()
