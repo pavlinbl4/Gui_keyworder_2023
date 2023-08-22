851f9e89002aa25de7322e682d20c9f4bf7b653a
evgeniy_MacBook <pavlinbl4@gmail.com>
2023-08-22 22:29:37 +0300

    create keywords from text via lemmatization

--- keyworder/lematization.py
+++ keyworder/lematization.py
@@ -1,21 +1,22 @@
 import pyperclip
 from pymystem3 import Mystem
 
-from input_window.best_keywords import keywords_opimization
-from input_window.checkbox_output import create_checkbox_list
-from input_window.new_input_window import create_input_window
-from keyworder.words_more_two_litters import extract_words
-from keyworder.write_keywords import write_keywords
+from Common.write_keywords import write_keywords
+from KW_2023_08.best_keywords import keywords_optimization
+from KW_2023_08.checkbox_output import create_checkbox_list
+from KW_2023_08.new_input_window import create_input_window
+from Common.only_words_from_string import extract_words_no_digits
+
 
 
 def lema(any_text):
-    lemmatized_words = extract_words(Mystem().lemmatize(any_text))
+    lemmatized_words = extract_words_no_digits(Mystem().lemmatize(any_text))
     return [word for word in lemmatized_words if len(word) > 2]
 
 def main():
-    first_test = create_input_window()[0]
+    first_test = create_input_window("Input your text for lemmatization")[0]
     all_words_list= lema(first_test)
-    no_doubles_str = keywords_opimization(", ".join(all_words_list))
+    no_doubles_str = keywords_optimization(", ".join(all_words_list))
     no_doubles_list = no_doubles_str.split(', ')
     selected_words_list = create_checkbox_list(no_doubles_list, "Select!!!")
     selected_words_str = ", ".join(selected_words_list)
