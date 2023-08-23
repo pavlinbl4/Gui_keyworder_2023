fb173045bdd3fdd4434f214eb69da83c0429cbf3
evgeniy_MacBook <pavlinbl4@gmail.com>
2023-08-23 23:00:32 +0300

    added automate text to keyword function

--- keyworder/lematization.py
+++ keyworder/lematization.py
@@ -8,14 +8,20 @@ from KW_2023_08.new_input_window import create_input_window
 from Common.only_words_from_string import extract_words_no_digits
 
 
+# automate text to keyword optimizator
+def lema_minus_bad_words(any_text: str):
+    all_words_list = lema(any_text)
+    return keywords_optimization(", ".join(all_words_list))
 
-def lema(any_text):
-    lemmatized_words = extract_words_no_digits(Mystem().lemmatize(any_text))
-    return [word for word in lemmatized_words if len(word) > 2]
+
+def lema(any_text: str) -> list:
+    lemmatized_words_lst = extract_words_no_digits(Mystem().lemmatize(any_text))
+    return [word for word in lemmatized_words_lst if len(word) > 2]
+
 
 def main():
     first_test = create_input_window("Input your text for lemmatization")[0]
-    all_words_list= lema(first_test)
+    all_words_list = lema(first_test)
     no_doubles_str = keywords_optimization(", ".join(all_words_list))
     no_doubles_list = no_doubles_str.split(', ')
     selected_words_list = create_checkbox_list(no_doubles_list, "Select!!!")
@@ -25,6 +31,6 @@ def main():
 
 
 if __name__ == '__main__':
-    # print(lema(
-    #     'крокодил крокодилов крокодилам Крокодилы динозавриками в аэропорту'))
-    main()
+    print(lema_minus_bad_words(
+        'крокодил крокодилов крокодилам Крокодилы динозавриками в аэропорту _РУССКИЙ ты для'))
+    # main()
