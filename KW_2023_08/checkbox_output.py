3bb87cf0ae52828a6cc732db1da1a4d5567ffbd6
evgeniy <pavlinbl4@gmail.com>
2023-08-23 09:53:08 +0300

    change position of the window

--- KW_2023_08/checkbox_output.py
+++ KW_2023_08/checkbox_output.py
@@ -3,8 +3,18 @@ import tkinter as tk
 
 # gui window to select items from list
 def create_checkbox_list(words: list, window_name: str) -> list:
-    root = tk.Tk()
-    root.title(window_name)
+    # Create window
+    window = tk.Tk()
+    window.title(window_name)
+
+    # Set the height of the window
+    window_width = 800
+    window_height = 400
+    screen_width = window.winfo_screenwidth()
+    screen_height = window.winfo_screenheight()
+    x = int(screen_width / 4)
+    y = int(screen_height / 2 - window_height / 2)
+    window.geometry(f"{window_width}x{window_height}+{x}+{y}")
 
     keywords = []
 
@@ -16,7 +26,7 @@ def create_checkbox_list(words: list, window_name: str) -> list:
         for x, keyword in enumerate(keywords):
             if keyword.get():
                 checked_words.append(words[x])
-        root.destroy()
+        window.destroy()
         return checked_words
 
     for i, word in enumerate(words):
@@ -25,20 +35,20 @@ def create_checkbox_list(words: list, window_name: str) -> list:
         row = i // max_col
         col = i % max_col
 
-        cb = tk.Checkbutton(root, text=word, variable=keywords[i])
+        cb = tk.Checkbutton(window, text=word, variable=keywords[i])
         cb.grid(row=row, column=col, sticky="w", padx=20, pady=15)
 
     def invert():
         for kword in keywords:
             kword.set(not kword.get())
 
-    invert_cb = tk.Checkbutton(root, text="Invert", command=invert)
+    invert_cb = tk.Checkbutton(window, text="Invert", command=invert)
     invert_cb.grid(columnspan=max_col, pady=40)
 
-    submit_btn = tk.Button(root, text="Submit", command=get_checked)
+    submit_btn = tk.Button(window, text="Submit", command=get_checked)
     submit_btn.grid(columnspan=max_col, pady=30)
 
-    root.mainloop()
+    window.mainloop()
     return checked_words
 
 
