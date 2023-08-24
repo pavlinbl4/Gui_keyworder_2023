5c53d1bbe5f747ae5ade3966ace8270fa2d94383
evgeniy_MacBook <pavlinbl4@gmail.com>
2023-08-24 22:30:07 +0300

    try to write correct file

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='Gui_keyworder_2023',
+    version='',
+    packages=['keyworder', 'KW_2023_08'],
+    url='',
+    license='',
+    author='evgeniy',
+    author_email='',
+    description=''
+)
