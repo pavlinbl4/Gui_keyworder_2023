cd96799402b534cf13903a425a4db6d24265467f
evgeniy <pavlinbl4@gmail.com>
2023-08-24 09:43:35 +0300

    refactoring

--- keyworder/lematization.py
+++ keyworder/lematization.py
@@ -8,7 +8,7 @@ from KW_2023_08.new_input_window import create_input_window
 from Common.only_words_from_string import extract_words_no_digits
 
 
-# automate text to keyword optimizator
+# automate text to keyword optimization
 def lema_minus_bad_words(any_text: str):
     all_words_list = lema(any_text)
     return keywords_optimization(", ".join(all_words_list))
@@ -21,7 +21,7 @@ def lema(any_text: str) -> list:
 
 def main():
     first_test = create_input_window("Input your text for lemmatization")[0]
-    all_words_list = lema(first_test)
+    all_words_list= lema(first_test)
     no_doubles_str = keywords_optimization(", ".join(all_words_list))
     no_doubles_list = no_doubles_str.split(', ')
     selected_words_list = create_checkbox_list(no_doubles_list, "Select!!!")
@@ -31,6 +31,6 @@ def main():
 
 
 if __name__ == '__main__':
-    print(lema_minus_bad_words(
-        'крокодил крокодилов крокодилам Крокодилы динозавриками в аэропорту _РУССКИЙ ты для'))
-    # main()
+    # print(lema(
+    #     'крокодил крокодилов крокодилам Крокодилы динозавриками в аэропорту'))
+    main()
